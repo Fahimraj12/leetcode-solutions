09b57b4474bdce444e2b3e14785fb9f432695bfe
Mo Fahim Raj <sca23085@gmail.com>
2026-09-11 21:00:11 +0530

    Time: 234 ms (54.45%), Space: 42.8 MB (76.58%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,79 @@
+class Solution {
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+
+                // Find an empty cell
+                if (board[row][col] == '.') {
+
+                    // Try digits 1 to 9
+                    for (char digit = '1'; digit <= '9'; digit++) {
+
+                        if (isValid(board, row, col, digit)) {
+
+                            // Place digit
+                            board[row][col] = digit;
+
+                            // Recursively solve
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            // Backtrack
+                            board[row][col] = '.';
+                        }
+                    }
+
+                    // No digit works
+                    return false;
+                }
+            }
+        }
+
+        // No empty cells -> Sudoku solved
+        return true;
+    }
+
+    private boolean isValid(
+        char[][] board,
+        int row,
+        int col,
+        char digit
+    ) {
+
+        // Check row
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] == digit) {
+                return false;
+            }
+        }
+
+        // Check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == digit) {
+                return false;
+            }
+        }
+
+        // Check 3 x 3 box
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+
+                if (board[i][j] == digit) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
